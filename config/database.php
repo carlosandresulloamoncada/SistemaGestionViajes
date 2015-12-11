7bdaa3907dc54301235f0d7de722b4f804f69182
carlosandresulloamoncada <caaulloa@alumnos.ubiobio.cl>
2015-12-11 17:26:47 -0300

    pabloc-config para migraciones de bd

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'icinf2'),
+			'username'  => env('DB_USERNAME', 'icinf2'),
+			'password'  => env('DB_PASSWORD', 'ICInf-252'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
